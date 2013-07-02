d5eaa199d3209886f577cc2d21d345ad1250ee9e
Jeanderson Barros Candido <jeandersonbc@gmail.com>
2013-07-02 16:08:18 -0400

    Added new class to handle paths

---
+++ src/csaferefactor/PathManager.java
@@ -0,0 +1,85 @@
+package csaferefactor;
+
+import csaferefactor.runnable.VMInitializerRunnable;
+
+
+/**
+ * Manages all paths to be used for communication.
+ * <p>
+ * <b>Important: All RMI configurations are performed by
+ * {@link SafeRefactorActivator#configureRMI()} and
+ * {@link VMInitializerRunnable}.</b>
+ * </p>
+ * 
+ * @author <a href="mailto:jeandersonbc@gmail.com">Jeanderson Candido</a>
+ * 
+ */
+public class PathManager {
+
+	private static PathManager instance;
+
+	/**
+	 * Singleton for this class.
+	 * 
+	 * @return An instance for this class.
+	 */
+	public static PathManager getInstance() {
+		if (instance == null) {
+			instance = new PathManager();
+		}
+		return instance;
+	}
+
+	private PathManager() {
+	}
+
+	/**
+	 * Adapts the given path to a friendly OS path.
+	 * 
+	 * @param path
+	 *            A string representing a path
+	 * @return The adapted path
+	 */
+	public String pathAdapter(String path) {
+		String operatingSystemID = System.getProperty("os.name");
+		if (operatingSystemID.toLowerCase().contains("windows")) {
+			return getWindowsFriendlyPath(path);
+		}
+		return getUnixFriendlyPath(path);
+	}
+
+	private String getUnixFriendlyPath(String path) {
+		// TODO
+		return null;
+	}
+
+	/**
+	 * Converts a path to a DOS 8.3 notation (using tilde notation).
+	 * <p>
+	 * Example:
+	 * <ol>
+	 * <li><code>C:\Users\Neo\Programming stuff\Matrix</code></li>
+	 * <li><code>C:\Users\Neo\Programming~1\Matrix</code></li>
+	 * </ol>
+	 * </p>
+	 * 
+	 * @param path
+	 *            The String to be converted
+	 * @return A friendly DOS 8.3 converted path
+	 */
+	private String getWindowsFriendlyPath(String path) {
+		/*
+		 * FIXME Code will crash in the following case:
+		 * 
+		 * (Path 1) "C:\Program Other files\The wrong
+		 * path\", (Path 2) "C:\Program Files\The right path\"
+		 * 
+		 * The right conversion would be "C:\Program~2\The~1\" since "Program~1"
+		 * refers to Path 1.
+		 * 
+		 * TODO Identify the right ID to be assigned (1, 2, 3...)
+		 */
+		return path.replaceAll("\\s[\\w\\s]*/", "~1/");
+	}
+
+}
---
+++ tests/csaferefactor/PathManagerTest.java
@@ -0,0 +1,40 @@
+package csaferefactor;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.Scanner;
+
+import junit.framework.Assert;
+
+import org.junit.Test;
+
+/**
+ * Tests for {@link PathManager}
+ * 
+ * @author <a href="mailto:jeandersonbc@gmail.com">Jeanderson Candido</a>
+ * 
+ */
+public class PathManagerTest {
+
+	private PathManager manager = PathManager.getInstance();
+
+	@Test
+	public void testWindowsPaths() throws FileNotFoundException {
+		Scanner in = new Scanner(new BufferedReader(new FileReader(
+				"./test_input/WinPathTransf.txt")));
+
+		// Conversion to a friendly DOS 8.3 name
+		String[] expectedResults = { "C:/Users/Neo/Documents/My~1/workspace/",
+				"C:/Users/Morpheus/Documents/How~1/examples/",
+				"C:/Users/Agent~1/References/Understanding~1/Chapter~1/",
+				"C:/Users/Trinity/Programming/workspace/" };
+
+		for (int i = 0; in.hasNext() && i < expectedResults.length; i++) {
+			String path = in.nextLine();
+			String result = manager.pathAdapter(path);
+			Assert.assertEquals(expectedResults[i], result);
+		}
+		in.close();
+	}
+}
