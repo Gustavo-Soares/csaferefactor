d6f3b1de1ba8e9409cdfc1ba4c625763e6fdd96f
Jeanderson Barros Candido <jeandersonbc@gmail.com>
2013-07-12 15:09:23 -0400

    Rollback and proper bugfix to issue #5
    Last commit solved the issue #5 but also changed code related to RMI.
    To avoid unoticed bugs, those changes related to RMI were deleted.
    
    This commit only contains bugfix for issue #5

--- src/csaferefactor/actions/SampleAction.java
+++ src/csaferefactor/actions/SampleAction.java
@@ -2,6 +2,8 @@ package csaferefactor.actions;
 
 import java.io.IOException;
 
+import javax.swing.JOptionPane;
+
 import org.eclipse.core.resources.IResourceChangeEvent;
 import org.eclipse.core.resources.IResourceChangeListener;
 import org.eclipse.core.resources.ResourcesPlugin;
@@ -23,25 +25,20 @@ import csaferefactor.listener.JavaElementChangedListener;
 import csaferefactor.listener.PartListener;
 
 /**
- * Our sample action implements workbench action delegate.
- * The action proxy will be created by the workbench and
- * shown in the UI. When the user tries to use the action,
- * this delegate will be created and execution will be 
- * delegated to it.
+ * Our sample action implements workbench action delegate. The action proxy will
+ * be created by the workbench and shown in the UI. When the user tries to use
+ * the action, this delegate will be created and execution will be delegated to
+ * it.
+ * 
  * @see IWorkbenchWindowActionDelegate
  */
 public class SampleAction implements IWorkbenchWindowActionDelegate {
 	private IWorkbenchWindow window;
-	/**
-	 * The constructor.
-	 */
-	public SampleAction() {
-	}
 
 	/**
-	 * The action has been activated. The argument of the
-	 * method represents the 'real' action sitting
-	 * in the workbench UI.
+	 * The action has been activated. The argument of the method represents the
+	 * 'real' action sitting in the workbench UI.
+	 * 
 	 * @see IWorkbenchWindowActionDelegate#run
 	 */
 	public void run(IAction action) {
@@ -54,8 +51,6 @@ public class SampleAction implements IWorkbenchWindowActionDelegate {
 	}
 
 	public void finishInit() throws IOException {
-		SafeRefactorActivator.getDefault().configureRMI();
-
 		IWorkbenchWindow activeWorkbenchWindow = SafeRefactorActivator
 				.getDefault().getWorkbench().getActiveWorkbenchWindow();
 
@@ -63,8 +58,15 @@ public class SampleAction implements IWorkbenchWindowActionDelegate {
 			return;
 		IWorkbenchPage page = activeWorkbenchWindow.getActivePage();
 
-		if (page == null)
+		// Checks if there's something to be listened
+		if (page == null || page.getActiveEditor() == null) {
+			JOptionPane.showMessageDialog(null,
+					"There is no active editor to watch",
+					"Couldn't initialize CSafeRefactor",
+					JOptionPane.OK_CANCEL_OPTION);
 			return;
+		}
+		SafeRefactorActivator.getDefault().configureRMI();
 
 		// set listener
 		JavaCore.addElementChangedListener(
@@ -88,27 +90,30 @@ public class SampleAction implements IWorkbenchWindowActionDelegate {
 		}
 
 	}
+
 	/**
-	 * Selection in the workbench has been changed. We 
-	 * can change the state of the 'real' action here
-	 * if we want, but this can only happen after 
-	 * the delegate has been created.
+	 * Selection in the workbench has been changed. We can change the state of
+	 * the 'real' action here if we want, but this can only happen after the
+	 * delegate has been created.
+	 * 
 	 * @see IWorkbenchWindowActionDelegate#selectionChanged
 	 */
 	public void selectionChanged(IAction action, ISelection selection) {
 	}
 
 	/**
-	 * We can use this method to dispose of any system
-	 * resources we previously allocated.
+	 * We can use this method to dispose of any system resources we previously
+	 * allocated.
+	 * 
 	 * @see IWorkbenchWindowActionDelegate#dispose
 	 */
 	public void dispose() {
 	}
 
 	/**
-	 * We will cache window object in order to
-	 * be able to provide parent shell for the message dialog.
+	 * We will cache window object in order to be able to provide parent shell
+	 * for the message dialog.
+	 * 
 	 * @see IWorkbenchWindowActionDelegate#init
 	 */
 	public void init(IWorkbenchWindow window) {
